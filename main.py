fa8451db7f7d10b65ca88bedc9b0f26a9d37073c
iiikey <133108668+42inchFlatscreenTV@users.noreply.github.com>
2025-05-28 02:10:23 +0000

    idek what i changed

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ from cave import Cave
 
 # create cave objects
 cavern = Cave("Cavern")
-grotto = Cave("Grotto")
+cavern.set_description("A dank and dirty cave ")
 dungeon = Cave("Dungeon")
-
-# set descriptions
-cavern.set_description("A dank and dirty cave.")
-grotto.set_description("A small cave with ancient graffiti.")
-dungeon.set_description("A large cave with a rack.")
+dungeon.set_description("A large cave with a rack")
+grotto = Cave("Grotto")
+grotto.set_description("A small cave with ancient graffiti")
 
 cavern.link_cave(dungeon, "south")
 grotto.link_cave(dungeon, "east")
 dungeon.link_cave(grotto, "west")
 
-cavern.describe()
+cavern.get_description()
